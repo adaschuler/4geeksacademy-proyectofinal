c7f5687b902126c16e2efb7ffabddea2bdda03a4
Cristian <cristian.gutie@gmail.com>
2021-08-08 13:33:10 -0400

    auth0 initial configuration

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -7,42 +7,44 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="container d-flex justify-content-center">
-			<div className="text-center mt-5">
-				<img className="img-fluid w-50" src={logo} />
-				<form>
-					<div className="form-group">
-						<input
-							type="email"
-							className="form-control"
-							id="exampleInputEmail1"
-							aria-describedby="emailHelp"
-							placeholder="Ingresa tu email"
-						/>
-						<small id="emailHelp" className="form-text text-muted">
-							{/* We'll never share your email with anyone else. */}
-						</small>
-					</div>
-					<div className="form-group">
-						<input
-							type="password"
-							className="form-control"
-							id="exampleInputPassword1"
-							placeholder="Password"
-						/>
-						<a className="underlineHover text-secondary" href="#">
-							¿Olvidaste tu clave?
-						</a>
-					</div>
+		<Auth0Provider>
+			<div className="container d-flex justify-content-center">
+				<div className="text-center mt-5">
+					<img className="img-fluid w-50" src={logo} />
+					<form>
+						<div className="form-group">
+							<input
+								type="email"
+								className="form-control"
+								id="exampleInputEmail1"
+								aria-describedby="emailHelp"
+								placeholder="Ingresa tu email"
+							/>
+							<small id="emailHelp" className="form-text text-muted">
+								{/* We'll never share your email with anyone else. */}
+							</small>
+						</div>
+						<div className="form-group">
+							<input
+								type="password"
+								className="form-control"
+								id="exampleInputPassword1"
+								placeholder="Password"
+							/>
+							<a className="underlineHover text-secondary" href="#">
+								¿Olvidaste tu clave?
+							</a>
+						</div>
+						<button type="submit" className="btn btn-outline-success btn-block">
+							Ingresar
+						</button>
+					</form>
+					<p />
 					<button type="submit" className="btn btn-outline-success btn-block">
-						Ingresar
+						Registrarse
 					</button>
-				</form>
-				<p />
-				<button type="submit" className="btn btn-outline-success btn-block">
-					Registrarse
-				</button>
+				</div>
 			</div>
-		</div>
+		</Auth0Provider>
 	);
 };
