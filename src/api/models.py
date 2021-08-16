3028c6e727010f17243fec379a38e3ccbfb44c37
adaschuler <adaschuler@gmail.com>
2021-08-16 21:52:09 +0000

    base de datos V01

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,79 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Plant(db.Model):
+    __tablename__ = 'plant'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    fantansy_name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    family_name = db.Column(db.String(120), unique=True, nullable=False)
+    idPlanttype = db.Column(db.Integer, db.ForeignKey('planttype.id'), nullable=False)
+    avatar = db.Column(db.String(120), unique=True, nullable=False)
+    idIndicator = db.Column(db.Integer, db.ForeignKey('indicator.id'), nullable=False)
+    user_photos = db.Column(db.String(120), unique=True, nullable=False)
+    water = db.Column(db.String(120), unique=True, nullable=False)
+    temperature_range = db.Column(db.String(120), unique=True, nullable=False)
+    idUser = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Plant %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "fantasy_name": self.fantasy_name,
+            "name": self.name,
+            "family_name": self.family_name,
+            "avatar": self.avatar,
+            "user_photos": self.user_photos,
+            "water": self.water,
+            "temperature_range": self.temperature_range
+        }
+
+class Indicator(db.Model):
+    __tablename__ = 'indicator'
+    id = db.Column(db.Integer, primary_key=True)
+    humidity = db.Column(db.String(120), unique=True, nullable=False)
+    irrigation = db.Column(db.String(120), unique=True, nullable=False)
+    plant = db.relationship('Plant', backref="indicator", lazy=True)
+
+    def __repr__(self):
+        return '<Indicator %r>' % self.humidity
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "humidity": self.humidity,
+            "irrigation": self.irrigation
+        }
+
+class PlantType(db.Model):
+    __tablename__ = 'planttype'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    plant = db.relationship('Plant', backref="planttype", lazy=True)
+
+    def __repr__(self):
+        return '<PlantType %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(80), unique=False, nullable=False)
+    avatar = db.Column(db.String(250), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    credential = db.Column(db.String(80), unique=False, nullable=False)
+    """ idPlant = db.Column(db.Integer, db.ForeignKey('plant.id'), nullable=False) """    
+    rol = db.Column(db.Boolean(), unique=False, nullable=False)
+    plant_relationship = db.relationship('Plant', backref="user", lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
